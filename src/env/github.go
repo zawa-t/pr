a8a1d4e0ceda6106b83ce94ff39eff9f158a2916
tsubasa.ozawa <t.ozawa.sake@gmail.com>
2024-11-05 22:42:17 +0900

    update:環境変数名の変更

--- src/env/github.go
+++ src/env/github.go
@@ -1,8 +1,8 @@
 package env
 
 var (
-	GithubRepositoryOwner   = getEnv("OWNER")
-	GithubRepository        = getEnv("REPO")
-	GithubPullRequestNumber = getEnv("PULL_NUMBER")
+	GithubRepositoryOwner   = getEnv("GITHUB_OWNER")
+	GithubRepository        = getEnv("GITHUB_REPO")
+	GithubPullRequestNumber = getEnv("GITHUB_PR_NUMBER")
 	GithubAPIToken          = getEnv("PR_COMMENTATOR_GITHUB_API_TOKEN")
 )
