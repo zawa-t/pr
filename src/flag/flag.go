2f5df96fc35fa75979927f17e9d5fae732ec5164
tsubasa.ozawa <t.ozawa.sake@gmail.com>
2024-11-05 21:55:27 +0900

    update:ディレクトリ構成の変更

---
+++ src/flag/flag.go
@@ -0,0 +1,102 @@
+package flag
+
+import (
+	"flag"
+	"log/slog"
+	"os"
+	"slices"
+
+	"github.com/zawa-t/pr-commentator/src/platform"
+)
+
+var usage = "Usage: pr-comment --name=[tool name] --ext=[file extension] --platform=[platform name] < inputfile"
+
+type Required struct {
+	Name, FileExtension, Platform string
+}
+
+type Optional struct {
+	CustomTextFormat, AlternativeText *string
+	Local                             bool
+}
+
+type Value struct {
+	Required
+	Optional
+}
+
+func NewValue() (value *Value) {
+	// Required
+	var name string
+	nameFlags := []string{"n", "name"}
+	for _, f := range nameFlags {
+		flag.StringVar(&name, f, "", "tool name. The flag is required.")
+	}
+
+	var fileExtension string
+	fileExtFlags := []string{"ext", "extension"}
+	for _, f := range fileExtFlags {
+		flag.StringVar(&fileExtension, f, "", "file extension. The flag is required.")
+	}
+
+	var platform string
+	flag.StringVar(&platform, "platform", "", "The flag is optional.")
+
+	// Optional
+	var customTextFormat string
+	customTextFormatFlags := []string{"cus", "custom-text-format"}
+	for _, f := range customTextFormatFlags {
+		flag.StringVar(&customTextFormat, f, "", "The flag is optional.")
+	}
+
+	var alternativeText string
+	alternativeTextFlags := []string{"alt", "alternative-text"}
+	for _, f := range alternativeTextFlags {
+		flag.StringVar(&alternativeText, f, "", "The flag is optional.")
+	}
+
+	var local bool
+	flag.BoolVar(&local, "local", false, "The flag is optional.")
+
+	flag.Parse()
+
+	value = &Value{
+		Required: Required{
+			Name:          name,
+			FileExtension: fileExtension,
+			Platform:      platform,
+		},
+	}
+
+	if customTextFormat != "" {
+		value.CustomTextFormat = &customTextFormat
+	}
+
+	if alternativeText != "" {
+		value.AlternativeText = &alternativeText
+	}
+
+	value.Local = local
+
+	value.validate()
+	return
+}
+
+func (r *Required) validate() {
+	if r.Name == "" || r.FileExtension == "" || r.Platform == "" {
+		slog.Error(usage)
+		os.Exit(1)
+	}
+
+	allowedExtensions := []string{"txt", "json"}
+	if !slices.Contains(allowedExtensions, r.FileExtension) {
+		slog.Error("The specified extension is not supported.")
+		os.Exit(1)
+	}
+
+	allowedPlatforms := []string{platform.Bitbucket, platform.Github}
+	if !slices.Contains(allowedPlatforms, r.Platform) {
+		slog.Error("The specified platform is not supported.")
+		os.Exit(1)
+	}
+}
---
+++ src/linter/golangci/format.go
@@ -0,0 +1,70 @@
+package golangci
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"log/slog"
+	"os"
+
+	"github.com/zawa-t/pr-commentator/src/flag"
+
+	"github.com/zawa-t/pr-commentator/src/platform"
+)
+
+func MakeInputDatas(flagValue flag.Value, stdin *os.File) (datas []platform.Raw) {
+	switch flagValue.FileExtension {
+	case "txt":
+		// input.Datas = readText(*flagValue)
+	case "json":
+		jsonData := decodeJSONData(stdin)
+		if flagValue.AlternativeText != nil {
+			jsonData.Issues = replaceText(*flagValue.AlternativeText, jsonData.Issues)
+		}
+		datas = makeInputDatas(flagValue.CustomTextFormat, jsonData.Issues)
+	default:
+		slog.Error("The specified extension could not be processed because it is not supported.")
+		os.Exit(1)
+	}
+	return
+}
+
+func replaceText(alternativeText string, issues []Issue) []Issue {
+	for i, v := range issues {
+		v.Text = alternativeText
+		issues[i] = v
+	}
+
+	return issues
+}
+
+func makeInputDatas(customTextFormat *string, issues []Issue) []platform.Raw {
+	datas := make([]platform.Raw, 0)
+	for _, v := range issues {
+		data := platform.Raw{
+			Linter:   v.FromLinter,
+			FilePath: v.Pos.Filename,
+			LineNum:  v.Pos.Line,
+			Summary:  v.Text,
+			Details:  v.Text,
+		}
+		if customTextFormat != nil {
+			tmpl, err := template.New("customTextFormat").Parse(*customTextFormat) // HACK: 本来はfor文のたびにParseをする必要はないため、for文の外でParseするようにできないか検討
+			if err != nil {
+				fmt.Println("Error parsing template:", err)
+				os.Exit(1)
+			}
+
+			var result bytes.Buffer
+			err = tmpl.Execute(&result, v)
+			if err != nil {
+				fmt.Println("Error executing template:", err)
+				os.Exit(1)
+			}
+			text := result.String()
+			data.CustomCommentText = &text // NOTE: 利用者から指定されたテキストに置き換え
+		}
+		datas = append(datas, data)
+	}
+	return datas
+}
