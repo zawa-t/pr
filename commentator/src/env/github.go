ee7f52e2b3cda639ef79247be756e973567d0c53
tsubasa.ozawa <t.ozawa.sake@gmail.com>
2024-11-10 23:01:12 +0900

    update:動作確認用

--- commentator/src/env/github.go
+++ commentator/src/env/github.go
@@ -8,37 +8,41 @@ import (
 )
 
 var (
-	GithubRepository        = getEnv("GITHUB_REPOSITORY")
-	GithubPullRequestNumber = getGithubPRNumber()
-	GithubAPIToken          = getEnv("PR_COMMENTATOR_GITHUB_API_TOKEN")
-	GithubCommitID          = getEnv("GITHUB_SHA")
+	GithubRepository                        = getEnv("GITHUB_REPOSITORY")
+	GithubPullRequestNumber, GithubCommitID = getGithubPRNumber()
+	GithubAPIToken                          = getEnv("PR_COMMENTATOR_GITHUB_API_TOKEN")
 )
 
+type Head struct {
+	SHA string `json:"sha"`
+}
+
 type PullRequest struct {
-	Number int `json:"number"`
+	Number int  `json:"number"`
+	Head   Head `json:"head"`
 }
 
 type Event struct {
 	PullRequest PullRequest `json:"pull_request"`
 }
 
-func getGithubPRNumber() string {
+// HACK: 以下のやり方はもう少し検討
+func getGithubPRNumber() (string, string) {
 	if Env.IsLocal() {
-		return getEnv("GITHUB_PR_NUMBER")
+		return getEnv("GITHUB_PR_NUMBER"), getEnv("GITHUB_SHA")
 	} else {
 		data, err := os.ReadFile(getEnv("GITHUB_EVENT_PATH"))
 		if err != nil {
 			fmt.Printf("Error reading event file: %v\n", err)
-			return ""
+			return "", ""
 		}
 
 		var event Event
 		if err := json.Unmarshal(data, &event); err != nil {
 			fmt.Printf("Error parsing JSON: %v\n", err)
-			return ""
+			return "", ""
 		}
 
-		fmt.Printf("Pull Request Number: %d\n", event.PullRequest.Number)
-		return strconv.Itoa(event.PullRequest.Number)
+		return strconv.Itoa(event.PullRequest.Number), event.PullRequest.Head.SHA
 	}
 }
